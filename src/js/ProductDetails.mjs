cd6ce140582f615ae5295a83ee71e18361bd520d
sunseehray <sabtirazona@byui.edu>
2023-11-10 14:34:34 +0800

    addToCart, error, one product only

--- src/js/ProductDetails.mjs
+++ src/js/ProductDetails.mjs
@@ -1,4 +1,4 @@
-import { setLocalStorage } from "./utils.mjs";
+import { getLocalStorage, setLocalStorage } from "./utils.mjs";
 
 function productDetailsTemplate(product) {
   return `<section class="product-detail"> <h3>${product.Brand.Name}</h3>
@@ -36,7 +36,11 @@ export default class ProductDetails {
       .addEventListener("click", this.addToCart.bind(this));
   }
   addToCart() {
-    setLocalStorage("so-cart", this.product);
+    console.log(this.product);
+    const cartItems = getLocalStorage() || [];
+    console.log(cartItems);
+    cartItems.push(this.product);
+    setLocalStorage("so-cart", cartItems);
   }
   renderProductDetails(selector) {
     const element = document.querySelector(selector);
