14a96701b50e6dfde16d6129625a1c8ea342ca25
IAmLHarris <har21091@byui.edu>
2023-11-21 00:12:53 -0600

    okay this is weird enough to commit and disect later

--- src/js/product-listing.js
+++ src/js/product-listing.js
@@ -2,8 +2,12 @@ import { loadHeaderFooter, getParam } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 import ProductList from "./ProductList.mjs";
 
+//strange that `loadHeaderFooter` is imported, but never used, but eh
+//I shall do it myself
+loadHeaderFooter();
+
 const category = getParam("category");
-import ProductList from "./ProductList.js";
+const dataSource = new ProductData();
 const listElement = document.querySelector(".product-list");
 const myList = new ProductList(category, dataSource, listElement);
 myList.init();
