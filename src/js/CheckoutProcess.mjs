8ab3c25fa74fc1bd443680d5404c21ca9ba85432
parien2022 <par22002@byui.edu>
2023-11-25 17:04:26 -0300

    ap

--- src/js/CheckoutProcess.mjs
+++ src/js/CheckoutProcess.mjs
@@ -2,7 +2,6 @@ import { getLocalStorage } from "./utils.mjs";
 import ExternalServices from "./ExternalServices.mjs";
 
 const services = new ExternalServices();
-
 function formDataToJSON(formElement) {
   const formData = new FormData(formElement),
     convertedJSON = {};
@@ -13,16 +12,15 @@ function formDataToJSON(formElement) {
 
   return convertedJSON;
 }
-// takes the items currently stored in the cart (localstorage) and returns them in a simplified form.
+
 function packageItems(items) {
-  // convert the list of products from localStorage to the simpler form required for the checkout process. Array.map would be perfect for this.
   const simplifiedItems = items.map((item) => {
     console.log(item);
     return {
       id: item.Id,
-      name: item.Name,
       price: item.FinalPrice,
-      quantity: item.CartQuantity,  
+      name: item.Name,
+      quantity: 1,
     };
   });
   return simplifiedItems;
@@ -33,76 +31,64 @@ export default class CheckoutProcess {
     this.key = key;
     this.outputSelector = outputSelector;
     this.list = [];
-    this.subTotal = 0;
     this.itemTotal = 0;
     this.shipping = 0;
     this.tax = 0;
     this.orderTotal = 0;
   }
-
   init() {
     this.list = getLocalStorage(this.key);
     this.calculateItemSummary();
-    this.calculateOrdertotal();
-    packageItems(this.list);
   }
-
   calculateItemSummary() {
-      const summaryElement = document.querySelector(this.outputSelector + ` #subtotal`);
-    // calculate and display the total amount of the items in the cart, and the number of items.
-      this.subTotal = this.list.reduce((total, item) => 
-          total + item.FinalPrice * item.CartQuantity, 0
-      );
-
-      summaryElement.innerHTML = this.subTotal;
-
-      this.itemTotal = this.list.reduce((total, item) =>
-          total + item.CartQuantity, 0
-      );
+    const summaryElement = document.querySelector(
+      this.outputSelector + " #cartTotal"
+    );
+    const itemNumElement = document.querySelector(
+      this.outputSelector + " #num-items"
+    );
+    itemNumElement.innerText = this.list.length;
+    // calculate the total of all the items in the cart
+    const amounts = this.list.map((item) => item.FinalPrice);
+    this.itemTotal = amounts.reduce((sum, item) => sum + item);
+    summaryElement.innerText = "$" + this.itemTotal;
   }
-
   calculateOrdertotal() {
-    // calculate the shipping and tax amounts. Then use them to along with the cart total to figure out the order total
-      this.tax = this.subTotal * .06;
-      this.shipping = 10 + 2 * (this.itemTotal - 1);
-
-      this.orderTotal = this.subTotal + this.tax + this.shipping;
-    // display the totals.
+    this.shipping = 10 + (this.list.length - 1) * 2;
+    this.tax = (this.itemTotal * 0.06).toFixed(2);
+    this.orderTotal = (
+      parseFloat(this.itemTotal) +
+      parseFloat(this.shipping) +
+      parseFloat(this.tax)
+    ).toFixed(2);
     this.displayOrderTotals();
   }
-
   displayOrderTotals() {
-    // once the totals are all calculated display them in the order summary page
-      const shipping = document.querySelector("#shipping");
-      const tax = document.querySelector("#tax");
-      const total = document.querySelector("#totalBill");
-
-      shipping.innerHTML = `$${this.shipping.toFixed(2)}`;
-      tax.innerHTML = `$${this.tax.toFixed(2)}`;
-      total.innerHTML = `$${this.orderTotal.toFixed(2)}`;
+    const shipping = document.querySelector(this.outputSelector + " #shipping");
+    const tax = document.querySelector(this.outputSelector + " #tax");
+    const orderTotal = document.querySelector(
+      this.outputSelector + " #orderTotal"
+    );
+    shipping.innerText = "$" + this.shipping;
+    tax.innerText = "$" + this.tax;
+    orderTotal.innerText = "$" + this.orderTotal;
   }
-
-
   async checkout() {
-    // build the data object from the calculated fields, the items in the cart, and the information entered into the form
     const formElement = document.forms["checkout"];
-    
+
     const json = formDataToJSON(formElement);
-    
+    // add totals, and item details
     json.orderDate = new Date();
-    json.totalBill = this.orderTotal;
+    json.orderTotal = this.orderTotal;
     json.tax = this.tax;
     json.shipping = this.shipping;
     json.items = packageItems(this.list);
     console.log(json);
-    
-    // call the checkout method in our ExternalServices module and send it our data object.
     try {
       const res = await services.checkout(json);
       console.log(res);
-    } catch(err) {
+    } catch (err) {
       console.log(err);
     }
   }
 }
-  
